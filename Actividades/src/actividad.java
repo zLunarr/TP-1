886ef0db210bcafb12494b6dee523f0e5ec65558
Flavio Demian Gonzalez <130401171+zLunarr@users.noreply.github.com>
2024-10-02 14:50:40 -0300

    Add files via upload

---
+++ Actividades/src/actividad.java
@@ -0,0 +1,87 @@
+import javax.swing.JOptionPane;
+public class actividad {
+
+	public static void main(String[] args) {
+		
+		/* 
+		 * 
+		 * - - ACTIVIDAD 1 --
+		 * float num1 = 5;
+		float num2 = 10;
+		
+		System.out.println("La suma es igual a " +(num1 + num2));
+		System.out.println("La resta es igual a " +(num1 - num2));
+		System.out.println("La multiplicacion es igual a " +(num1 * num2));
+		System.out.println("La division es igual a " +(num1 / num2));
+		System.out.println("El modulo es igual a " +(num1%num2)); */
+		
+		
+		
+		/* 
+		 * 
+		 * -- ACTIVIDAD 2 --
+		 * int num1 = 20;
+		int num2 = 6;
+		
+		if(num1 > num2) {
+			System.out.println("El numero 1 es el mayor");
+		}
+		else {
+			System.out.println("El numero 2 es el mayor");
+		}
+		
+		if(num1 == num2) {
+			System.out.println("Los numeros son iguales");
+		} */
+
+		
+		/*
+		 * 
+		 * -- ACTIVIDAD 3 --
+		 * String bienvenida = "Flavio";
+		
+		
+		System.out.println("Bienvenido a Java" +bienvenida);*/
+		
+		
+	/*                  -- ACTIVIDAD 4 -- 
+	
+	String bienvenida=JOptionPane.showInputDialog("Cual es tu nombre");
+	
+	System.out.println("Bienvenido a java " +bienvenida); */
+		
+		
+		/*		-- ACTIVIDAD 5 --
+				
+		String area=JOptionPane.showInputDialog("Cual es tu radio");
+		double radio = Double.parseDouble(area);
+		double area2=Math.PI * Math.pow(radio, 2);
+
+		System.out.println("El area de tu circulo es " +area2); */
+		
+		
+	/*	             -- ACTIVIDAD 6 --
+		
+		String numero=JOptionPane.showInputDialog("Cual es su numero");
+		int numerito = Integer.parseInt(numero);
+		
+		if(numerito%2 == 0) {
+			System.out.println("El numero es efectivamente divisibile por 2");
+			
+		}
+		
+		else {
+			System.out.println("El numero no es divisible por 2 perdedor");
+		} */
+		
+		
+		/* -- ACTIVIDAD 7 --
+		String codigo=JOptionPane.showInputDialog("Inserte un codigo del ASCII");
+		int ascii = Integer.parseInt(codigo);
+		
+		char caracter = (char)ascii;
+		
+		System.out.println(caracter); */
+	}
+
+}
